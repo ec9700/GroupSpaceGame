117dfc5d189a89e62c9e40d98d29c659d3531c9e
NSchlueter <ns9100@students.inghamisd.org>
2023-02-28 13:47:56 -0500

    step3

--- core/src/com/mygdx/game/Components/PlayerController.java
+++ core/src/com/mygdx/game/Components/PlayerController.java
@@ -3,8 +3,13 @@ package com.mygdx.game.Components;
 import com.badlogic.gdx.graphics.Cursor;
 import com.rectgdx.Component;
 import com.rectgdx.KeyBinder;
+import com.rectgdx.TimeManager;
 
 public class PlayerController extends Component {
+
+    private float speed = 0;
+    private final float ACCELERATE = 10;
+
     @Override
     public void initial() {
         //Initial() runs once
@@ -17,10 +22,15 @@ public class PlayerController extends Component {
     public void update() {
         //Update() runs every frame
         //Puts things that need to update here
-        System.out.println(parent.getPositionVec2());
-       parent.lookAt(KeyBinder.getMousePosVec2());
-       parent.move(parent.getNewTrajectoryVec2(KeyBinder.getMousePosVec2()).scl(3));
+        if (KeyBinder.getKeyDown("accelerate")) {
+            speed= speed+ ACCELERATE * TimeManager.getDeltaTime();
+        }
+        else if (KeyBinder.getKeyDown("decelerate")) {
+            speed= speed- ACCELERATE * TimeManager.getDeltaTime();
+        }
+        parent.lookAt(KeyBinder.getMousePosVec2());
+        parent.move(parent.getNewTrajectoryVec2(KeyBinder.getMousePosVec2()).scl(speed));
         //You can access the player by using parent ^^^
-
     }
+
 }
